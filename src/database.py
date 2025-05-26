6048791e19247b00d03c46ba8cdc2de63a010b1d
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-05-26 20:35:07 +0300

    added the basic python structure

---
+++ src/database.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+def get_connection():
+    pass
+
+def add_product(name: str, category: str, quantity: int, price: float) -> None:
+    pass
+
+def update_stock(product_id: int, quantity_change: int) -> None:
+    pass
+
+def view_inventory() -> list[dict]:
+    pass
+
+def remove_product(product_id: int) -> None:
+    pass
+
